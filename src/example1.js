b211593f1f8dc5f982f48b50ad16b08da1dd4c94
RJ Salvador <rjsalvadorr@gmail.com>
2019-07-30 09:39:30 -0400

    Added 2nd test

--- src/example1.js
+++ src/example1.js
@@ -2,4 +2,8 @@ function sum(a, b) {
   return a + b;
 }
 
-export { sum };
+function subtract(a, b) {
+  return a - b;
+}
+
+export { sum, subtract };
--- src/example1.test.js
+++ src/example1.test.js
@@ -1,5 +1,9 @@
-import { sum } from './example1';
+import { sum, subtract } from './example1';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('subtracts 4 from 9 to equal 5', () => {
+  expect(subtract(9, 4)).toBe(5);
 });
